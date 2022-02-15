3d0e33d996ed313b625c7dc2608dd71347e394a5
zeuspod <josseluism@gmail.com>
2022-02-14 22:03:13 -0400

    add comandos , enviar , total

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 from distutils import command
+from email import message
+from sys import prefix
+from unicodedata import name
 import discord
 from discord import embeds
 from discord.colour import Color
@@ -16,21 +19,40 @@ load_dotenv(dotenv_path=env_path)
 dst = os.getenv('DISCORD_TOKEN')
 
 bot = discord.Client()
+bot = commands.Bot(command_prefix='$')	
 
 @bot.event
 async def on_ready():
     print('Bot is ready')
+    await bot.change_presence(activity=discord.Game(name='calcular tasas :)'))
 
-@bot.event
-async def on_message(message):
-    prefix='$'
 
-    if message.content.startswith(prefix):
-        command = message.content.strip(prefix)
-        monto = calculo(command)
-        total = str(monto)
-        embed = discord.Embed(title="Calculo venta Paypal", description=f"El monto a enviar con comision es de: {total}", color=discord.Colour.blue())
-        await message.channel.send(embed=embed)
+@bot.command()
+async def comandos(ctx):
+    embed = discord.Embed(title="Comandos", description="Comandos disponibles", color = discord.Color.blue())
+    embed.add_field(name="$enviar", value="Calcula el monto a enviar con comision", inline=False)
+    embed.add_field(name="$total", value="Calcula el monto en bs a recibir", inline=False)
+    await ctx.send(embed=embed)
+#evento envio de precio con la tasa
+  
+@bot.command()
+async def enviar(ctx, monto):
+    amount = float(monto)
+    total = calculo(amount)
+    embed = discord.Embed(title="Calculo venta Paypal", description=f"El monto a enviar con comision es de: {total}", color=discord.Colour.blue(), timestamp=datetime.datetime.utcnow())
+    embed.add_field(name="fuente:" , value="**F8Network**")
+    await ctx.send(embed=embed)
+
+
+
+@bot.command()
+async def total(ctx, num1: float , num2: float):
+    total = calculo_venta(num1, num2)
+    embed = discord.Embed(title="Calculo venta Paypal", description=f"El monto a recibir es de: **{total}** bs", color=discord.Colour.blue(), timestamp=datetime.datetime.utcnow())
+    embed.add_field(name="Tasa comprador:" , value=f"**{num1}**")
+    embed.add_field(name="Paypal neto enviado:" , value=f"**{num2}**")
+    embed.add_field(name="fuente:" , value="**F8Network**")
+    await ctx.send(embed=embed)
 
 load_dotenv()
 bot.run(os.getenv('DISCORD_TOKEN'))
--- paypal.py
+++ paypal.py
@@ -1,8 +1,18 @@
 #calculo de las tasas y comisiones para pagos paypal
 #tasa 5.4% 
 #comision 0.30$
-def calculo(command):
-    monto = float(command)
+def calculo(monto):
+    monto = float(monto)
     total= round((monto+0.30)/0.946,2)
     return total
 
+
+def calculo_venta(buyer_price, quantity):
+    buyer_price = float(buyer_price)
+    quantity = float(quantity)
+    whit_tax = calculo(quantity)
+    total = round (whit_tax * buyer_price, 2)
+    return total
+
+
+    
